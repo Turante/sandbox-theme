cf52dcb881ff21916fd219bdefaca0edbed9b33e
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2007-02-18 20:57:22 +0000

    Initial upload of 0.6.1 version

---
+++ trunk/page.php
@@ -0,0 +1,27 @@
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content" class="hfeed">
+
+<?php the_post() ?>
+
+			<div id="post-<?php the_ID(); ?>" class="<?php sandbox_post_class() ?>">
+				<h2 class="entry-title"><?php the_title(); ?></h2>
+				<div class="entry-content">
+<?php the_content() ?>
+
+<?php link_pages('<div class="page-link">'.__('Pages: ', 'sandbox'), '</div>', 'number'); ?>
+
+<?php edit_post_link(__('Edit this entry.', 'sandbox'),'<p class="edit-link">','</p>') ?>
+
+				</div>
+			</div><!-- .post -->
+
+<?php /* Add a custom field with key "comments" (value is ignored) to turn on comments for a page! */ ?>
+<?php if ( get_post_custom_values('comments') ) comments_template() ?>
+
+		</div><!-- #content .hfeed -->
+	</div><!-- #container -->
+
+<?php get_sidebar() ?>
+<?php get_footer() ?>
