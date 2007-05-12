2bbf9b08924f5c131bf8b541f779f256a9717f2e
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2007-05-12 22:51:33 +0000

    - Added spans to laquo; and raquo; but removed them from form fields - Removed rel="me" except for external links on author.php - Ugh; working on the home link, but it's not fixed - Added t[n] for trackback counting

--- category.php
+++ category.php
@@ -7,8 +7,8 @@
 			<div class="archive-meta"><?php if ( !(''== category_description()) ) : echo apply_filters('archive_meta', category_description()); endif; ?></div>
 
 			<div id="nav-above" class="navigation">
-				<div class="nav-previous"><?php next_posts_link(__('Older posts', 'sandbox')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts', 'sandbox')) ?></div>
+				<div class="nav-previous"><?php next_posts_link(__('<span style="meta-nav">&laquo;</span> Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span style="meta-nav">&raquo;</span>', 'sandbox')) ?></div>
 			</div>
 
 <?php while (have_posts()) : the_post(); ?>
@@ -17,11 +17,11 @@
 				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'sandbox'), get_the_title()) ?>" rel="bookmark"><?php the_title() ?></a></h3>
 				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s &#8211; %2$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></div>
 				<div class="entry-content">
-<?php the_excerpt(''.__('Read More', 'sandbox').'') ?>
+<?php the_excerpt(''.__('Read More &raquo;', 'sandbox').'') ?>
 
 				</div>
 				<div class="entry-meta">
-					<span class="author vcard"><?php printf(__('By %s', 'sandbox'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="View all posts by ' . $authordata->display_name . '" rel="me">'.get_the_author().'</a>') ?></span>
+					<span class="author vcard"><?php printf(__('By %s', 'sandbox'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="View all posts by ' . $authordata->display_name . '">'.get_the_author().'</a>') ?></span>
 					<span class="meta-sep">|</span>
 <?php if ( $cats_meow = sandbox_cats_meow(', ') ) : /* only show categories other than the one queried */ ?>
 					<span class="cat-links"><?php printf(__('Also posted in %s', 'sandbox'), $cats_meow) ?></span>
@@ -35,8 +35,8 @@
 <?php endwhile; ?>
 
 			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link(__('Older posts', 'sandbox')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts', 'sandbox')) ?></div>
+				<div class="nav-previous"><?php next_posts_link(__('<span style="meta-nav">&laquo;</span> Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span style="meta-nav">&raquo;</span>', 'sandbox')) ?></div>
 			</div>
 
 		</div><!-- #content -->
