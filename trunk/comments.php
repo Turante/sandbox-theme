92c43fd032a8af76ac467fd9f6fe517454f5a646
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2007-05-21 03:36:01 +0000

    Fixed escaping in unapproved message '/t/t/t' etc

--- trunk/comments.php
+++ trunk/comments.php
@@ -32,11 +32,11 @@ foreach ( $comments as $comment )
 <?php if ( get_comment_type() == "comment" ) : ?>
 						<li id="comment-<?php comment_ID() ?>" class="<?php sandbox_comment_class() ?>">
 							<div class="comment-author vcard"><span class="fn n"><?php comment_author_link() ?></span></div>
-<?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your comment is awaiting moderation.</span>\n', 'sandbox') ?>
 							<div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'sandbox'),
 										get_comment_date(),
 										get_comment_time(),
 										'#comment-' . get_comment_ID() ); edit_comment_link(__('Edit', 'sandbox'), ' <span class="edit-link"><span class="meta-sep">|</span> ', '</span>'); ?></div>
+<?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'sandbox') ?>
 							<?php comment_text() ?>
 						</li>
 <?php endif; /* if ( get_comment_type() == "comment" ) */ ?>
