f9bca27da1580fb5bd83f3b18197d938b7017018
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2008-03-29 18:24:57 +0000

    # Changed default avatar size from 64 to 32 # Removed generator LINK from header.php since WP is inserting it automatically now # Removed random (?) .content-column class from archives.php

--- tags/1.5/archives.php
+++ tags/1.5/archives.php
@@ -16,13 +16,13 @@ Template Name: Archives Page
 <?php the_content(); ?>
 
 					<ul id="archives-page" class="xoxo">
-						<li id="category-archives" class="content-column">
+						<li id="category-archives">
 							<h3><?php _e('Archives by Category', 'sandbox') ?></h3>
 							<ul>
 								<?php wp_list_categories('optioncount=1&feed=RSS&title_li=&show_count=1') ?> 
 							</ul>
 						</li>
-						<li id="monthly-archives" class="content-column">
+						<li id="monthly-archives">
 							<h3><?php _e('Archives by Month', 'sandbox') ?></h3>
 							<ul>
 								<?php wp_get_archives('type=monthly&show_post_count=1') ?>
