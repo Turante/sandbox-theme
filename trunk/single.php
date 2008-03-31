dbece8c03dedea9058a4338261301540a66b00b8
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2008-03-31 15:46:57 +0000

    # Included image.php; first pass at sandbox_gallery_shortcode() function # Updated style.css for galleries # Updated Chinese translation (#89)

--- trunk/single.php
+++ trunk/single.php
@@ -3,7 +3,8 @@
 	<div id="container">
 		<div id="content">
 
-<?php the_post(); ?>
+<?php the_post() ?>
+
 			<div id="nav-above" class="navigation">
 				<div class="nav-previous"><?php previous_post_link('%link', '<span class="meta-nav">&laquo;</span> %title') ?></div>
 				<div class="nav-next"><?php next_post_link('%link', '%title <span class="meta-nav">&raquo;</span>') ?></div>
