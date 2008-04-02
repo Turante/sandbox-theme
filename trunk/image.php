68303160915dd809a1d4f758c242c039948238e5
swallick <swallick@a848c381-991a-0410-adc5-cf79ac2c82a1>
2008-04-02 12:10:08 +0000

    # Now using inline element to give % width in style attribute when using gallery # Updated style.css

--- trunk/image.php
+++ trunk/image.php
@@ -9,7 +9,7 @@
 			<div id="post-<?php the_ID(); ?>" class="<?php sandbox_post_class() ?>">
 				<h3 class="entry-title"><?php the_title() ?></h3>
 				<div class="entry-content">
-					<div class="entry-attachment"><a href="<?php echo wp_get_attachment_url($post->ID); ?>" title="<?php echo wp_specialchars( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo basename($post->guid) ?></a></div>
+					<div class="entry-attachment"><a href="<?php echo wp_get_attachment_url($post->ID); ?>" title="<?php echo wp_specialchars( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo wp_get_attachment_image( $post->ID, 'large' ); ?></a></div>
 					<div class="entry-caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); ?></div>
 <?php the_content(''.__('Read More <span class="meta-nav">&raquo;</span>', 'sandbox').'') ?>
 
